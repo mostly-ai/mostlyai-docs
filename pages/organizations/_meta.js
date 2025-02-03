0cae01197215378648e8486ab255c2d83fd15ff7
Zach Tilev <103522279+zachtil@users.noreply.github.com>
2025-02-03 10:30:29 +0200

    refine

--- pages/organizations/_meta.js
+++ pages/organizations/_meta.js
@@ -1,6 +1,7 @@
 export default {
     "create": "Create an organization",
     "manage-assets": "Manage assets",
+    "manage-asset-visibility": "Manage asset visibility",
     "manage-members": "Manage members",
     "delete": "Delete an organization"
 }
