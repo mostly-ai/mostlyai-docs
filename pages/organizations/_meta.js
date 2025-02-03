2594834e0920190e19feeb2b681325c6369f56bc
Zach Tilev <103522279+zachtil@users.noreply.github.com>
2025-02-03 11:10:33 +0200

    add

--- pages/organizations/_meta.js
+++ pages/organizations/_meta.js
@@ -1,5 +1,6 @@
 export default {
     "create": "Create an organization",
+    "edit-settings": "Edit organization settings",
     "manage-assets": "Manage assets",
     "manage-asset-visibility": "Manage asset visibility",
     "manage-members": "Manage members",
