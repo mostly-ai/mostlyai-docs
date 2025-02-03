d3b5674c1b54c413dbdf68c734de37a0fdcabae9
Zach Tilev <103522279+zachtil@users.noreply.github.com>
2025-02-03 19:06:42 +0200

    add status pills to docs

---
+++ components/status-pills.tsx
@@ -0,0 +1,61 @@
+import Image from 'next/image'
+
+export const Canceled = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-canceled.svg" alt="MOSTLY AI Status pill - Canceled" width={80} height={30} />
+    )
+}
+
+export const Failed = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-failed.svg" alt="MOSTLY AI Status pill - Failed" width={60} height={30} />
+    )
+}
+
+export const Generating = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-canceled.svg" alt="MOSTLY AI Status pill - Canceled" width={80} height={30} />
+    )
+}
+
+export const InProgress = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-in-progress.svg" alt="MOSTLY AI Status pill - Canceled" width={95} height={30} />
+    )
+}
+
+export const New = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-new.svg" alt="MOSTLY AI Status pill - Canceled" width={53} height={30} />
+    )
+}
+
+export const OnHold = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-on-hold.svg" alt="MOSTLY AI Status pill - Canceled" width={80} height={30} />
+    )
+}
+
+export const Paused = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-paused.svg" alt="MOSTLY AI Status pill - Canceled" width={80} height={30} />
+    )
+}
+
+export const Queued = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-queued.svg" alt="MOSTLY AI Status pill - Canceled" width={65} height={30} />
+    )
+}
+
+export const Ready = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-ready.svg" alt="MOSTLY AI Status pill - Ready" width={60} height={30} />
+    )
+}
+
+export const Continue = () => {
+    return (
+        <Image className="card-icon" src="/docs/icons/mostly-ai/statuses/status-pill-continue.svg" alt="MOSTLY AI Status pill - Continue" width={75} height={30} />
+    )
+}
