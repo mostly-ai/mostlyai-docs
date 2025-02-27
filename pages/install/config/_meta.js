4f641a9882f3540102af52d3a07ce2a8922c3465
zachtil <103522279+zachtil@users.noreply.github.com>
2025-02-27 18:56:13 +0200

    docs(update): SSL/TSL domain certificate configuration (#38)

--- pages/install/config/_meta.js
+++ pages/install/config/_meta.js
@@ -2,6 +2,6 @@ export default {
     "compute-resources": "Compute resources",
     "external-postgresql-db": "External PostgreSQL database",
     "internal-image-repo": "Internal image repository",
-    "domain-ssl-cert": "Domain SSL certificate",
+    "domain-tls-cert": "Domain TLS certificate",
     "ingress-controllers": "Ingress controllers",
 }
