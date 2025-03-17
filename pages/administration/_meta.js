f44251469f078148800b96ad16192362090c9b6c
zachtil <103522279+zachtil@users.noreply.github.com>
2025-03-17 17:26:15 +0200

    docs(feat): add instructions for PingOne identity provider (#50)

--- pages/administration/_meta.js
+++ pages/administration/_meta.js
@@ -2,6 +2,7 @@ export default {
     "compute": "Compute",
     "models": "Models",
     "users-groups": "Users and groups",
+    "identity-providers": "Identity providers",
     "storage-policy": "Storage policy",
     "backup-restore": "Backup and restore"
 }
