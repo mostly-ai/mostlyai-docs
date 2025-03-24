95ef70e053df3cd980b26aa9fdc58a98d5b3520a
zachtil <103522279+zachtil@users.noreply.github.com>
2025-03-24 16:54:47 +0200

    docs(release): 4.4.1 - New connector access types (#58)

--- pages/connectors/use/_meta.js
+++ pages/connectors/use/_meta.js
@@ -1,4 +1,5 @@
 export default {
+    "access-types": "Connector access types",
     "data-source": "Use as a data source",
     "data-destination": "Use as a data destination",
     "local-db": "Use a local database"
