cf914d8cfff50ceb09290207cfd02fcdb86c98d3
Viktor Vereckei <viktor.vereckei@mostly.ai>
2025-06-25 18:15:20 +0200

    Tie GTM initialization to ID being set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { initTagManager } from "../utils/gtm";
 import '../styles/global.css';
 
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
 
   useEffect(() => {
     initTagManager();
