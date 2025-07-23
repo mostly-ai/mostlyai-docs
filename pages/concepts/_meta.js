8d8fa2f22ff1eac8012972d1ebc88a866103d670
Kenneth Hamilton <kenneth.hamilton@mostly.ai>
2025-07-23 14:46:45 +0200

    add data for everyone

--- pages/concepts/_meta.js
+++ pages/concepts/_meta.js
@@ -1,4 +1,5 @@
 export default {
+  'data-for-everyone': 'Data for everyone',
   'what-is-synthetic-data': 'What is synthetic data?',
   'privacy-protection': 'Privacy protection',
   'context-processing-multi-table': 'Context processing in multi-table datasets',
