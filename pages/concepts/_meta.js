1b2ef6ae25fd6c34e39e2d771c5acf9e76ba2c11
Ken <kenneth.hamilton@mostly.ai>
2025-07-24 12:44:48 +0200

    what are credits concept article (#137)
    * what are credits concept article
    
    * lint
    
    * clean up
    
    * word choice

--- pages/concepts/_meta.js
+++ pages/concepts/_meta.js
@@ -1,5 +1,6 @@
 export default {
   'data-for-everyone': 'Data for everyone',
+  'what-are-credits': 'What are credits?',
   'what-is-synthetic-data': 'What is synthetic data?',
   'privacy-protection': 'Privacy protection',
   'context-processing-multi-table': 'Context processing in multi-table datasets',
