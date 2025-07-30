b1806037926a8a02230ea77df933d6fc43e29c04
Ken <kenneth.hamilton@mostly.ai>
2025-07-30 14:45:54 +0200

    refactor users and groups (#146)
    * refactor users and groups
    
    * finalize

--- pages/administration/_meta.js
+++ pages/administration/_meta.js
@@ -1,7 +1,7 @@
 export default {
   compute: 'Computes',
   models: 'Models',
-  'users-groups': 'Users and groups',
+  'users-groups': 'Users',
   'identity-providers': 'Identity providers',
   'storage-policy': 'Storage policy',
   'backup-restore': 'Backup and restore',
