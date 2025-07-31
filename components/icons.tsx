3bc28077f0a19262f8b0daff9c5b79f44722bbae
Kenneth Hamilton <kenneth.hamilton@mostly.ai>
2025-07-31 11:54:01 +0200

    update source icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -303,5 +303,5 @@ export const APIIcon = () => {
 };
 
 export const GitHubIcon = () => {
-  return <Image className="card-icon" src="/icons/third-party/github.svg" alt="GitHub Icon" width={26} height={26} style={{ display: 'inline', marginTop: '-2px' }} />;
+  return <Image className="card-icon" src="/icons/third-party/github.svg" alt="GitHub Icon" width={26} height={26} style={{ display: 'inline'}} />;
 };
