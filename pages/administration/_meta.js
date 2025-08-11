5b98344907c933383ec9e5787f59e65dfcd8e92f
Kenneth Hamilton <kenneth.hamilton@mostly.ai>
2025-08-11 13:20:34 +0200

    remove invalid article

--- pages/administration/_meta.js
+++ pages/administration/_meta.js
@@ -2,7 +2,6 @@ export default {
   compute: 'Computes',
   models: 'Models',
   'users-groups': 'Users',
-  'identity-providers': 'Identity providers',
   'storage-policy': 'Storage policy',
   'backup-restore': 'Backup and restore',
 };
